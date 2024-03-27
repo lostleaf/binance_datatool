642eac1cc4a4f02b3d242186c8b8bf0ece3da33a
lostleaf <lostleaf@me.com>
2024-03-27 10:40:56 +0800

    Add compare

--- bhds/cli.py
+++ bhds/cli.py
@@ -1,8 +1,10 @@
 import asyncio
 
 from .aws_candle import convert_aws_candle_csv, get_aws_candle, verify_all_candle
-
 from .quantclass_candle import convert_quantclass_candle_csv
+from .compare import compare_aws_quantclass_candle
+
+
 class Bhds:
     """
     Binance Historical Data Service
@@ -28,8 +30,16 @@ class Bhds:
         Converts and merges downloaded candlestick data into Pandas Feather format.
         """
         convert_aws_candle_csv(typ, time_interval)
+
     def convert_quantclass_candle_csv(self, typ, time_interval):
         """
         Converts quantclass candlestick data into Pandas Feather format.
         """
         convert_quantclass_candle_csv(typ, time_interval)
+
+    def compare_aws_quantclass_candle(self, typ, time_interval, *symbols):
+        """
+        Compare AWS candle with Quantclass
+        """
+        for symbol in symbols:
+            compare_aws_quantclass_candle(typ, time_interval, symbol)
---
+++ bhds/compare.py
@@ -0,0 +1,56 @@
+import logging
+import os
+
+import pandas as pd
+
+from config import Config
+
+
+def compare_aws_quantclass_candle(type_, time_interval, symbol):
+    logging.info('Compare AWS with Quantclass candlestick %s %s %s', type_, time_interval, symbol)
+
+    path_aws = os.path.join(Config.BINANCE_DATA_DIR, 'candle_feather', type_, time_interval, f'{symbol}.fea')
+    logging.info('Path %s AWS', path_aws)
+
+    path_qtc = os.path.join(Config.BINANCE_QUANTCLASS_DIR, 'candle_feather', type_, time_interval, f'{symbol}.fea')
+    logging.info('Path %s Quantclass', path_qtc)
+
+    df_aws = pd.read_feather(path_aws)
+    logging.info('Time %s -- %s AWS', df_aws['candle_begin_time'].min(), df_aws['candle_begin_time'].max())
+
+    df_qtc = pd.read_feather(path_qtc)
+    logging.info('Time %s -- %s Quantclass', df_qtc['candle_begin_time'].min(), df_qtc['candle_begin_time'].max())
+
+    begin_ts = max(df_aws['candle_begin_time'].min(), df_qtc['candle_begin_time'].min())
+    end_ts = min(df_aws['candle_begin_time'].max(), df_qtc['candle_begin_time'].max())
+    logging.info('Time %s -- %s', begin_ts, end_ts)
+
+    df_aws = df_aws[df_aws['candle_begin_time'].between(begin_ts, end_ts)]
+    logging.info('Trimmed shape %s AWS', df_aws.shape)
+
+    df_qtc = df_qtc[df_qtc['candle_begin_time'].between(begin_ts, end_ts)]
+    logging.info('Trimmed shape %s Quantclass', df_qtc.shape)
+
+    df = df_aws.join(df_qtc.set_index('candle_begin_time'), on='candle_begin_time', rsuffix='_qtc')
+
+    cols = [
+        'open', 'high', 'low', 'close', 'volume', 'quote_volume', 'trade_num', 'taker_buy_base_asset_volume',
+        'taker_buy_quote_asset_volume'
+    ]
+
+    error_begin_time = None
+    for c in cols:
+        df['diff'] = (df[c] - df[f'{c}_qtc']).abs()
+        max_diff = df['diff'].max()
+        diff_num = (df['diff'] > 1e-4).sum()
+        logging.info('Column: %s, max diff %f, diff num %d', c, max_diff, diff_num)
+        if max_diff > 1e-4:
+            error_begin_time = df[df['diff'] == max_diff].iloc[0]['candle_begin_time']
+        df.drop(columns='diff', inplace=True)
+
+    if error_begin_time is not None:
+        df_err = pd.concat([
+            df_aws.loc[df_aws['candle_begin_time'] == error_begin_time, cols],
+            df_qtc.loc[df_qtc['candle_begin_time'] == error_begin_time, cols]
+        ])
+        logging.error('%s\n%s', error_begin_time, df_err)
