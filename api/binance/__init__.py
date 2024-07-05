f56feacf0007249ecb3473afa25f6eea671abd5c
lostleaf <lostleaf@me.com>
2024-07-05 15:35:39 +0800

    Fix imports

--- api/binance/__init__.py
+++ api/binance/__init__.py
@@ -1,5 +1,6 @@
 from .binance_market_restful import (BinanceBaseMarketApi, BinanceMarketCMDapi,
                                      BinanceMarketSpotApi, BinanceMarketUMFapi,
                                      create_binance_market_api)
-from .binance_market_ws import (get_coin_futures_kline_socket,
-                                get_usdt_futures_kline_socket)
+from .binance_market_ws import (get_coin_futures_multi_candlesticks_socket,
+                                get_coin_futures_multi_candlesticks_socket,
+                                get_spot_multi_candlesticks_socket)
