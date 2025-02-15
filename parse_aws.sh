b47b10f8c38c2d2a2bb21fb79779b09f078727f8
lostleaf <lostleaf@me.com>
2025-02-15 15:02:11 +0800

    Update parse script and environment

--- parse_aws.sh
+++ parse_aws.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 python bhds.py aws_kline parse-type-all spot 1m
+python bhds.py api_data download-aws-missing-kline spot 1m
+
 python bhds.py aws_kline parse-type-all um_futures 1m
+python bhds.py api_data download-aws-missing-kline um_futures 1m
+
 python bhds.py aws_kline parse-type-all cm_futures 1m
+python bhds.py api_data download-aws-missing-kline cm_futures 1m
