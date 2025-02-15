56e59dd62f44181fc0a42ebd7842b6f897f8db04
lostleaf <lostleaf@me.com>
2025-02-15 22:30:37 +0800

    Format entry

--- bhds.py
+++ bhds.py
@@ -7,11 +7,19 @@ from api.app import app as api_data
 
 app = typer.Typer()
 
-app.add_typer(aws_funding, name='aws_funding', help='Commands for maintaining Binance AWS funding rate data.')
-app.add_typer(aws_liquidation, name='aws_liquidation',
-              help='Commands for maintaining Binance AWS liquidation snapshot data.')
-app.add_typer(aws_kline, name='aws_kline', help='Commands for maintaining Binance AWS K-line data.')
-app.add_typer(api_data, name='api_data', help='Commands for maintaining Binance API data.')
-
-if __name__ == '__main__':
+app.add_typer(
+    aws_funding,
+    name="aws_funding",
+    help="Commands for maintaining Binance AWS funding rate data.",
+)
+app.add_typer(
+    aws_kline,
+    name="aws_kline",
+    help="Commands for maintaining Binance AWS K-line data.",
+)
+app.add_typer(api_data, name="api_data", help="Commands for maintaining Binance API data.")
+app.add_typer(
+    aws_liquidation, name="aws_liquidation", help="Commands for maintaining Binance AWS liquidation snapshot data."
+)
+if __name__ == "__main__":
     app()
