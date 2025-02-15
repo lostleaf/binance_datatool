2fd777da770baadaf8d832082bde36ec60f7d6ac
lostleaf <lostleaf@me.com>
2025-02-15 14:38:45 +0800

    Download and verify 1m only

--- download_verify_aws.sh
+++ download_verify_aws.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-python bhds.py aws_kline download-spot 1m 5m 1h
-python bhds.py aws_kline download-spot 1m 5m 1h
-python bhds.py aws_kline verify-type-all spot 1m 5m 1h
+python bhds.py aws_kline download-spot 1m
+python bhds.py aws_kline download-spot 1m
+python bhds.py aws_kline verify-type-all spot 1m
 
-python bhds.py aws_kline download-um-futures 1m 5m 1h
-python bhds.py aws_kline download-um-futures 1m 5m 1h
-python bhds.py aws_kline verify-type-all um_futures 1m 5m 1h
+python bhds.py aws_kline download-um-futures 1m
+python bhds.py aws_kline download-um-futures 1m
+python bhds.py aws_kline verify-type-all um_futures 1m
 
-python bhds.py aws_kline download-cm-futures 1m 5m 1h
-python bhds.py aws_kline download-cm-futures 1m 5m 1h
-python bhds.py aws_kline verify-type-all cm_futures 1m 5m 1h
+python bhds.py aws_kline download-cm-futures 1m
+python bhds.py aws_kline download-cm-futures 1m
+python bhds.py aws_kline verify-type-all cm_futures 1m
 
