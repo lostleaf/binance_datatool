039f9caf5800a816cc84f532106c728d209d7b7d
leomajesty <359755853@qq.com>
2025-02-18 10:44:56 +0800

    add liquidation data for bhds.

--- bhds.py
+++ bhds.py
@@ -1,12 +1,15 @@
 import typer
 
 from aws.funding.app import app as aws_funding
+from aws.liquidation.app import app as aws_liquidation
 from aws.kline.app import app as aws_kline
 from api.app import app as api_data
 
 app = typer.Typer()
 
 app.add_typer(aws_funding, name='aws_funding', help='Commands for maintaining Binance AWS funding rate data.')
+app.add_typer(aws_liquidation, name='aws_liquidation',
+              help='Commands for maintaining Binance AWS liquidation snapshot data.')
 app.add_typer(aws_kline, name='aws_kline', help='Commands for maintaining Binance AWS K-line data.')
 app.add_typer(api_data, name='api_data', help='Commands for maintaining Binance API data.')
 
