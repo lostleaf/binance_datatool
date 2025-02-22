7adf1f2b84edc85c6bff01d02ceafafac3f8dbe7
lostleaf <lostleaf@me.com>
2025-02-22 20:15:59 +0800

    Add help and format

--- api/app.py
+++ api/app.py
@@ -10,7 +10,7 @@ from config import ContractType, TradeType
 
 app = typer.Typer()
 
-HTTP_PROXY = os.getenv('HTTP_PROXY', None) or os.getenv('http_proxy', None)
+HTTP_PROXY = os.getenv("HTTP_PROXY", None) or os.getenv("http_proxy", None)
 
 
 @app.command()
@@ -30,9 +30,9 @@ def download_kline(
     ],
     http_proxy: Annotated[Optional[str], typer.Option(help="HTTP proxy address")] = HTTP_PROXY,
 ):
-    '''
+    """
     Download Binance klines for specific symbol and dates from Kline API
-    '''
+    """
     sym_dts = [(symbol, dt) for dt in dts]
     asyncio.run(api_download_kline(trade_type, time_interval, sym_dts, http_proxy))
 
@@ -44,10 +44,10 @@ def download_aws_missing_kline(
         str,
         typer.Argument(help="The time interval for the K-lines, e.g., '1m', '5m', '1h'."),
     ],
-    overwrite: bool = False,
+    overwrite: Annotated[bool, typer.Option(help="Whether to overwrite existing files")] = False,
     http_proxy: Annotated[Optional[str], typer.Option(help="HTTP proxy address")] = HTTP_PROXY,
 ):
-    '''
+    """
     Download Binance kline data from the Kline API for the provided trade_type that have missing dates in AWS
-    '''
+    """
     asyncio.run(api_download_aws_missing_kline(trade_type, time_interval, overwrite, http_proxy))
