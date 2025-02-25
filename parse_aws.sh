d16c680e1e27d8ceabe70940ebc3247d4284afca
lostleaf <lostleaf@me.com>
2025-02-25 18:48:45 +0800

    Correct parse

--- parse_aws.sh
+++ parse_aws.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 python bhds.py aws_kline parse-type-all spot 1m
-python bhds.py api_data download-aws-missing-kline spot 1m
+python bhds.py api_data download-aws-missing-kline-type spot 1m
 python bhds.py generate merged-split-kline-type-all spot 1m --split-gaps --with-vwap
 
 python bhds.py aws_kline parse-type-all um_futures 1m
-python bhds.py api_data download-aws-missing-kline um_futures 1m
+python bhds.py api_data download-aws-missing-kline-type um_futures 1m
 
 python bhds.py aws_kline parse-type-all cm_futures 1m
-python bhds.py api_data download-aws-missing-kline cm_futures 1m
+python bhds.py api_data download-aws-missing-kline-type cm_futures 1m
